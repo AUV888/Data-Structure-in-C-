73e9cc7dd4b0135540233ca24fd3d92d05c1666f
AUV888 <bigsticks114514@gmail.com>
2025-10-09 23:15:09 +0800

    Fix: completed functions in set.c and added 2 functions for debug use

--- 1009 assignment/set.c
+++ 1009 assignment/set.c
@@ -20,9 +20,12 @@ int SetFind(set s, elem val);
 int SetSize(set s);
 int SetUnion(set sa, set sb, set *sc);
 int SetIntersection(set sa, set sb, set *sc);
+void TestFunctions(void);   // For testing the program only
+void DebugPrintSet(set *s); // For debug use only
 
 int main()
 {
+    TestFunctions();
     return 0;
 }
 
@@ -91,25 +94,108 @@ int SetErase(set *s, elem val)
 
 int SetClear(set *s)
 {
+    if (s == NULL || s->data == NULL)
+        return 0;
+    s->len = 0;
     return 1;
 }
 
 int SetFind(set s, elem val)
 {
-    return 1;
+    if (s.data == NULL)
+        return 0;
+    for (int i = 0; i < s.len; i++)
+    {
+        if (s.data[i] == val)
+            return 1;
+    }
+    return 0;
 }
 
 int SetSize(set s)
 {
-    return 1;
+    if (s.data == NULL)
+        return 0;
+    return s.len;
 }
 
 int SetUnion(set sa, set sb, set *sc)
 {
+    if (sc == NULL || sc->data == NULL || sa.data == NULL || sb.data == NULL)
+        return 0;
+    if (sc->data)
+        free(sc->data);
+    sc->data = malloc((sa.len + sb.len) * sizeof(elem));
+    if (sc->data == NULL)
+        return 0;
+    sc->vol = sa.len + sb.len;
+    memcpy(sc->data, sa.data, sa.len * sizeof(elem));
+    sc->len = sa.len;
+    int top = sa.len;
+    for (int i = 0; i < sb.len; i++)
+    {
+        if (!SetFind(*sc, sb.data[i]))
+        {
+            sc->data[top] = sb.data[i];
+            sc->len++;
+        }
+    }
     return 1;
 }
 
 int SetIntersection(set sa, set sb, set *sc)
 {
+    if (sc == NULL || sc->data == NULL || sa.data == NULL || sb.data == NULL)
+        return 0;
+    if (sc->data)
+        free(sc->data);
+    int minimum_vol = sa.len > sb.len ? sb.len : sa.len;
+    sc->data = malloc(minimum_vol * sizeof(elem));
+    sc->len = 0;
+    if (sc->data == NULL)
+        return 0;
+    for (int i = 0; i < sb.len; i++)
+    {
+        if (SetFind(sa, sb.data[i]))
+        {
+            sc->data[sc->len] = sb.data[i];
+            sc->len++;
+        }
+    }
     return 1;
+}
+
+void TestFunctions(void) // For testing the program only
+{
+    return;
+}
+
+void DebugPrintSet(set *s) // For debug use only
+{
+    if (s == NULL)
+    {
+        fprintf(stderr, "No Such Set\n");
+        return;
+    }
+    if (s->data == NULL)
+    {
+        fprintf(stderr, "Data in Set Not Available\n");
+        return;
+    }
+    if (s->vol <= s->len || s->vol == 0)
+    {
+        fprintf(stderr, "The Set Has No Volume\n");
+        return;
+    }
+    if (s->len == 0 && s->vol >= 0)
+    {
+        printf("Empty Set\n");
+        return;
+    }
+    for (int i = 0; i < s->len; i++)
+    {
+        printf("%d ", s->data[i]);
+    }
+    printf("\n");
+    return;
 }
