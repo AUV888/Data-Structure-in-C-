946b70f41a6252adc76a4cdd1a26602e3089c5fa
fire bunny <2959308391@qq.com>
2025-10-14 22:09:43 +0800

    修复: 修改 DestroySet 函数的参数类型以支持双重指针

--- Experiment 1/setA.c
+++ Experiment 1/setA.c
@@ -9,7 +9,7 @@ struct SET
 };
 typedef struct SET set;
 int InitSet(set *);
-int DestroySet(set *);
+int DestroySet(set **);
 int SetInsert(set *, int);
 int SetErase(set *, int);
 int SetClear(set *);
@@ -23,9 +23,27 @@ int main()
 }
 int InitSet(set *s)
 {
+    if (s == NULL)
+        return 0;
+    s->data = (char *)malloc(8192 * sizeof(char));
+    if (s->data == NULL)
+        return 0;
+    s->len = 0;
+    return 1;
 }
-int DestroySet(set *s)
+int DestroySet(set **s)
 {
+    if ((*s) == NULL || (*s)->data == NULL)
+        return 0;
+    if ((*s)->data != NULL)
+    {
+        free((*s)->data);
+        (*s)->data = NULL;
+    }
+    free(*s);
+    *s = NULL;
+    (*s)->len = 0;
+    return 1;
 }
 int SetInsert(set *s, int val)
 {
@@ -35,20 +53,49 @@ int SetInsert(set *s, int val)
     int col = val - (val >> 3) << 3;
     char mask = 0x80 >> col;
     s->data[row] |= mask;
+    s->len++;
     return 1;
 }
 int SetErase(set *s, int val)
 {
+    if (s == NULL || s->data == NULL || val >= SIZE_OF_SET)
+        return 0;
+    int row = val >> 3;
+    int col = val - (val >> 3) << 3;
+    char mask = ~(0x80 >> col);
+    s->data[row] &= mask;
+    s->len--;
+    return 1;
 }
 int SetClear(set *s)
 {
+    if (s == NULL || s->data == NULL)
+        return 0;
+    for (int i = 0; i < 8192; i++)
+        s->data[i] = 0;
+    s->len = 0;
+    return 1;
 }
 int SetFind(set *s, int val)
 {
+    if (s == NULL || s->data == NULL || val >= SIZE_OF_SET)
+        return 0;
+    int row = val >> 3;
+    int col = val - (val >> 3) << 3;
+    char mask = 0x80 >> col;
+    if (!!(s->data[row] & mask))
+        return 1;
+    else
+        return 0;
 }
 int SetSize(set s, int val)
 {
+    return  s.len;
 }
 int SetIntersection(set sa, set sb, set *sc)
 {
+    for(int i = 0; i < 8192; i++){
+        sc->data[i] = sa.data[i] & sb.data[i];
+        
+    return 1;
 }
