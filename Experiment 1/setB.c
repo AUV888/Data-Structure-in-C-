aeab4f7209c86cb1e44dbee62445ac167c9b9ed2
AUV888 <bigsticks114514@gmail.com>
2025-10-15 23:03:19 +0800

    Add: implement RandomInsert, RandomFind, and RandomErase functions in main

--- Experiment 1/setB.c
+++ Experiment 1/setB.c
@@ -24,9 +24,22 @@ int SetClear(set *);
 int SetFind(set *, char *);
 int SetSize(set, char *);
 int SetUnion(set *, set *, set *);
+void RandomInsert(set *);
+void RandomFind(set *);
+void RandomErase(set *);
 
 int main()
 {
+    set *a, *b, *c;
+    InitSet(&a);
+    InitSet(&b);
+    InitSet(&c);
+    RandomInsert(a);
+    RandomErase(a);
+    RandomFind(a);
+    DestroySet(&a);
+    DestroySet(&b);
+    DestroySet(&c);
     return 0;
 }
 int InitSet(set **s)
@@ -52,4 +65,16 @@ int SetSize(set s, char *t)
 }
 int SetUnion(set *sa, set *sb, set *sc)
 {
+}
+void RandomInsert(set *s)
+{
+    return;
+}
+void RandomFind(set *s)
+{
+    return;
+}
+void RandomErase(set *s)
+{
+    return;
 }
