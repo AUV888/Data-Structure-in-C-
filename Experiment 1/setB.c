354fdc05024fffcc0f17cdb107adc15aa09d0019
AUV888 <bigsticks114514@gmail.com>
2025-10-16 17:09:20 +0800

    Add: file input handling for RandomInsert, RandomFind, and RandomErase functions

--- Experiment 1/setB.c
+++ Experiment 1/setB.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct NODE
 {
@@ -35,8 +36,8 @@ int main()
     InitSet(&b);
     InitSet(&c);
     RandomInsert(a);
-    RandomErase(a);
     RandomFind(a);
+    RandomErase(a);
     DestroySet(&a);
     DestroySet(&b);
     DestroySet(&c);
@@ -68,13 +69,100 @@ int SetUnion(set *sa, set *sb, set *sc)
 }
 void RandomInsert(set *s)
 {
+    FILE *fp = fopen("insert.in", "r");
+    if (!fp)
+    {
+        fprintf(stderr, "cannot open insert.in\n");
+        return;
+    }
+    int len, cnt = 0;
+    while (fscanf(fp, "%d", &len) == 1)
+    {
+        if (cnt % 2000 == 0)
+            printf("Inserting: %d / 500000\r", cnt);
+        if (len == -1)
+        {
+            break;
+        }
+        char *word = (char *)malloc(len + 1);
+        if (!word)
+        {
+            fprintf(stderr, "Memory allocation failed\n");
+            break;
+        }
+        fscanf(fp, "%s", word);
+        word[len] = '\0';
+        SetInsert(s, word);
+        cnt++;
+    }
+    printf("\n");
+    fclose(fp);
     return;
 }
 void RandomFind(set *s)
 {
+    FILE *fp = fopen("find.in", "r");
+    if (!fp)
+    {
+        fprintf(stderr, "cannot open find.in\n");
+        return;
+    }
+    int len, cnt = 0, found = 0;
+    while (fscanf(fp, "%d", &len) == 1)
+    {
+        if (cnt % 2000 == 0)
+            printf("Finding: %d / 100000\r", cnt);
+        if (len == -1)
+        {
+            break;
+        }
+        char *word = (char *)malloc(len + 1);
+        if (!word)
+        {
+            fprintf(stderr, "Memory allocation failed\n");
+            break;
+        }
+        fscanf(fp, "%s", word);
+        word[len] = '\0';
+        if(SetFind(s,word))
+            found++;
+        
+        cnt++;
+    }
+    printf("\nFound: %d / 100000\n");
+    fclose(fp);
     return;
 }
 void RandomErase(set *s)
 {
+    FILE *fp = fopen("erase.in", "r");
+    if (!fp)
+    {
+        fprintf(stderr, "cannot open erase.in\n");
+        return;
+    }
+    int len, cnt = 0, erased = 0;
+    while (fscanf(fp, "%d", &len) == 1)
+    {
+        if (cnt % 2000 == 0)
+            printf("Erasing: %d / 500000\r", cnt);
+        if (len == -1)
+        {
+            break;
+        }
+        char *word = (char *)malloc(len + 1);
+        if (!word)
+        {
+            fprintf(stderr, "Memory allocation failed\n");
+            break;
+        }
+        fscanf(fp, "%s", word);
+        word[len] = '\0';
+        if (SetErase(s, word))
+            erased++;
+        cnt++;
+    }
+    printf("\nErased: %d / 500000\n", erased);
+    fclose(fp);
     return;
 }
