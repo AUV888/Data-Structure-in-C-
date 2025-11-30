bda6b56f6ad81b9118507e36ff7cf47967920377
AUV888 <bigsticks114514@gmail.com>
2025-11-30 20:55:33 +0800

    Fix: the fatal error in Is_BST has been fixed by using inorder traverse

--- 1124 assignment/Is_BST.c
+++ 1124 assignment/Is_BST.c
@@ -5,20 +5,29 @@ typedef struct BiTNode
     struct BiTNode *left, *right;
 } BiTNode, *BiTree;
 
-int Is_BST(BiTree b)
+void In_Order_Traverse(BiTree b, int *result, int *len)
 {
     if (!b)
-        return 1;
-    else if (!b->left && !b->right)
-        return 1;
-    else if (b->left && !b->right && b->left->data < b->data && Is_BST(b->left))
-        return 1;
-    else if (!b->left && b->right && b->right->data > b->data && Is_BST(b->right))
-        return 1;
-    else if (b->left && b->right && Is_BST(b->left) && Is_BST(b->right) && b->left->data < b->data && b->right->data > b->data)
-        return 1;
-    else
-        return 0;
+        return;
+    In_Order_Traverse(b->left, result, len);
+    result[*len] = b->data;
+    (*len)++;
+    In_Order_Traverse(b->right, result, len);
+    return;
+}
+
+int Is_BST(BiTree b)
+{
+    int result[10000], len = 0, glob_max = 0x80000000;
+    In_Order_Traverse(b, result, &len);
+    for (int i = 0; i < len; i++)
+    {
+        if (result[i] <= glob_max)
+            return 0;
+        else
+            glob_max = result[i];
+    }
+    return 1;
 }
 
 int main()
